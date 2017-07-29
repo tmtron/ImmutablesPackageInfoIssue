4b7c68b59c02b42eb8b743ff9f56453b1d9aca07
Martin <martin@tmtron.com>
2017-07-29 20:26:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/AndroidIm.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import org.immutables.value.Value;
+
+@Value.Immutable
+public interface AndroidIm {
+    String id();
+}
---
+++ lib/src/main/java/com/example/JavaIm.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.immutables.value.Value;
+
+@Value.Immutable
+public interface JavaIm {
+    String id();
+    String test();
+}
